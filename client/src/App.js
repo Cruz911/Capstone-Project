d910e408caa083baad6a79fea1f8d9d226f8f01b
cruz911 <joelchadambuka@gmail.com>
2021-01-12 02:04:03 +0200

    commit 1

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SignIn from './components/Login/SignIn';
+function App() {
+  return (
+    <div className="App" >
+      
+      <SignIn/>
+
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ server/app.js
@@ -0,0 +1,6 @@
+var express = require("express")
+const app = express()
+
+app.listen(5000, ()=>{
+    console.log("Done")
+})
